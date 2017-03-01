c070bc0ceb013fc9e4124d8969a8c1776885bd1d
Heipiao <lsl8315@gmail.com>
2017-03-01 09:51:11 +0000

    change

--- src/main/scala/test1.scala
+++ src/main/scala/test1.scala
@@ -12,38 +12,47 @@ import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.sql.Row
 
 // Create Spark session
-val sparkSession = SparkSession.builder
+
+
+import org.apache.spark.sql.SparkSession
+// define main method (scala entry point)
+object Decision {
+  def main(args: Array[String]): Unit = {
+
+    val sparkSession = SparkSession.builder
     .master("local[1]")
     .appName("Decision Tree example")
     .getOrCreate()
 
 // Load the data
-val text = sparkSession.sparkContext.textFile("files/spambase.data")
+	val text = sparkSession.sparkContext.textFile("files/spambase.data")
 
 // Separate into array
-val data = text.map(line => line.split(',').map(_.toDouble)).map(t => LabeledPoint(t(57), Vectors.dense(t.take(57))))
+	val data = text.map(line => line.split(',').map(_.toDouble)).map(t => LabeledPoint(t(57), Vectors.dense(t.take(57))))
 
 // Split the data into training and test sets (30% held out for testing)
-val splits = data.randomSplit(Array(0.7, 0.3))
-val (trainingData, testData) = (splits(0), splits(1))
+	val splits = data.randomSplit(Array(0.7, 0.3))
+	val (trainingData, testData) = (splits(0), splits(1))
 
 // Train a DecisionTree model.
 //  Empty categoricalFeaturesInfo indicates all features are continuous.
-val numClasses = 2
-val categoricalFeaturesInfo = Map[Int, Int]()
-val impurity = "gini"
-val maxDepth = 5
-val maxBins = 32
+	val numClasses = 2
+	val categoricalFeaturesInfo = Map[Int, Int]()
+	val impurity = "gini"
+	val maxDepth = 5
+	val maxBins = 32
 
-val model = DecisionTree.trainClassifier(trainingData, numClasses, categoricalFeaturesInfo,
-  impurity, maxDepth, maxBins)
+	val model = DecisionTree.trainClassifier(trainingData, numClasses, categoricalFeaturesInfo,
+  	impurity, maxDepth, maxBins)
 
 // Evaluate model on test instances and compute test error
-val labelAndPreds = testData.map { point =>
-  val prediction = model.predict(point.features)
-  (point.label, prediction)
+	val labelAndPreds = testData.map { point =>
+  	val prediction = model.predict(point.features)
+  	(point.label, prediction)
 }
 
-val testErr = labelAndPreds.filter(r => r._1 != r._2).count().toDouble / testData.count()
-println("Test Error = " + testErr)
-println("Learned classification tree model:\n" + model.toDebugString)
+	val testErr = labelAndPreds.filter(r => r._1 != r._2).count().toDouble / testData.count()
+	println("Test Error = " + testErr)
+	println("Learned classification tree model:\n" + model.toDebugString)
+  }
+}
